170270e11d0eff9483abc642eab11b9fe50b8dee
Aron Ayub <55284959+AronAyub@users.noreply.github.com>
2021-09-04 01:52:29 +0300

    DHT LCD Arduino Relay Ac control
    Demonstration of DHT LCD Relay integration of Arduino in Proteus software with C code.

---
+++ DHT Arduino LCD proteus Relay AC control/code/DHT_1.ino
@@ -0,0 +1,106 @@
+// include the library code:
+#include <LiquidCrystal.h>
+
+// initialize the library by associating any needed LCD interface pin
+// with the arduino pin number it is connected to
+const int rs = 12, en = 11, d4 = 6, d5 = 5, d6 = 4, d7 = 3;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+
+
+#include "DHT.h"
+
+#define DHTPIN 2     // what digital pin we're connected to
+
+// Uncomment whatever type you're using!
+#define DHTTYPE DHT11   // DHT 11
+
+// Connect pin 1 (on the left) of the sensor to +5V
+// NOTE: If using a board with 3.3V logic like an Arduino Due connect pin 1
+// to 3.3V instead of 5V!
+// Connect pin 2 of the sensor to whatever your DHTPIN is
+// Connect pin 4 (on the right) of the sensor to GROUND
+// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+
+// Initialize DHT sensor.
+// Note that older versions of this library took an optional third parameter to
+// tweak the timings for faster processors.  This parameter is no longer needed
+// as the current DHT reading algorithm adjusts itself to work on faster procs.
+DHT dht(DHTPIN, DHTTYPE);
+
+void setup() {
+  lcd.begin(16, 2);
+  Serial.begin(9600);
+  Serial.println("DHTxx test!");
+  pinMode(8, OUTPUT);
+
+  dht.begin();
+}
+
+void loop() {
+  // Wait a few seconds between measurements.
+  delay(2000);
+
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read temperature as Fahrenheit (isFahrenheit = true)
+  float f = dht.readTemperature(true);
+
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || isnan(f)) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+
+  // Compute heat index in Fahrenheit (the default)
+  float hif = dht.computeHeatIndex(f, h);
+  // Compute heat index in Celsius (isFahreheit = false)
+  float hic = dht.computeHeatIndex(t, h, false);
+  
+  //Serial.println("success");
+  Serial.print("Humidity: ");
+  Serial.print(h);
+  Serial.print(" %\t");
+  Serial.print("Temperature: ");
+  Serial.print(t);
+  Serial.print(" *C ");
+  Serial.print(f);
+  Serial.print(" *F\t");
+  Serial.print("Heat index: ");
+  Serial.print(hic);
+  Serial.print(" *C ");
+  Serial.print(hif);
+  Serial.println(" *F");
+  
+  
+ 
+
+  if (h > 79) 
+  
+   {
+    digitalWrite(8,HIGH);
+    lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Humidity ");
+  lcd.setCursor(0, 1);
+  lcd.print("HIGH 'ON' : ");
+  lcd.setCursor(10, 1);
+  lcd.print(h);
+  delay(500);
+   }
+  else
+   {
+    digitalWrite(8,LOW);
+lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Humidity ");
+  lcd.setCursor(0, 1);
+  lcd.print("Less 'OFF' : ");
+  lcd.setCursor(10, 1);
+  lcd.print(h);
+  delay(500);
+  
+   }
+}
